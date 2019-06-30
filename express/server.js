8cb0f31380dab3ac9d1f4e98e1afff9cf8fa0eda
Ethan Miller <ethan.romans5.8@gmail.com>
2019-06-29 22:01:55 -0500

    add views to root

--- express/server.js
+++ express/server.js
@@ -1,32 +1,33 @@
 'use strict';
 const express = require('express');
+const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 const router = express.Router();
 
-let views = 'https://github.com/king-melchizedek/netlify-mvc-app/tree/master/views';
+let views = path.join(__dirname, '../views');
 
 router.get('/', (req, res) => {
   res.sendFile('index.html', { root: views });
 });
 
-// router.get('/page1', (req, res) => {
-//   res.sendFile('page1', { root: views })
-// });
+router.get('/page1', (req, res) => {
+  res.sendFile('page1', { root: views })
+});
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 
 // Development template.
 
-// app.get('/', (req, res) => {
-//   res.render('index')
-// });
+app.get('/', (req, res) => {
+  res.render('index')
+});
 
-// app.get('/page1', (req, res) => {
-//   res.render('page1')
-// });
+app.get('/page1', (req, res) => {
+  res.render('page1')
+});
 
 module.exports = app;
 module.exports.handler = serverless(app);
