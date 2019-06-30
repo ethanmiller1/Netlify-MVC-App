2ac110f1e0d1e8ba7415946576f9287443e32f12
Ethan Miller <ethan.romans5.8@gmail.com>
2019-06-29 21:43:41 -0500

    add page 1

--- server-local.js
+++ server-local.js
@@ -1,5 +1,10 @@
 'use strict';
 
 const app = require('./express/server');
+const path = require('path');
+
+// Load View Engine
+app.set('views', path.join(__dirname, 'pug'));
+app.set('view engine', 'pug');
 
 app.listen(3000, () => console.log('Local app listening on port 3000!'));
