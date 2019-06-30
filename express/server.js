229d0c18a07ee5eff8e3484b57e17170f084aa91
Ethan Miller <ethan.romans5.8@gmail.com>
2019-06-29 20:47:52 -0500

    render pug on server

--- express/server.js
+++ express/server.js
@@ -6,9 +6,7 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+  res.render('index')
 });
 app.get('/', (req, res) => {
   res.render('index')
